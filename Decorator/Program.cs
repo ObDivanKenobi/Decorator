9efeb7b48f6a637f9ed34f4ff16bad24ba7746e9
Ilia Manshin <ilia.manshin@yandex.ru>
2018-04-11 00:21:18 +0300

    done

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherProvider;
 
 namespace Decorator
 {
@@ -10,7 +11,67 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            int tries = 5;
+            SomeProprietartyWeatherProvider provider = new SomeProprietartyWeatherProvider();
+            RetryingWeatherProvider decorated = new RetryingWeatherProvider(provider, 5, 0);
+            string city = "SomeCity";
 
+            bool baseProviderFailed = false,
+                decoratorSucceeded = false;
+
+            while(!baseProviderFailed & !decoratorSucceeded)
+            {
+                try
+                {
+                    provider.GetWeather(city);
+                    baseProviderFailed = false;
+                }
+                catch
+                {
+                    baseProviderFailed = true;
+                }
+
+                try
+                {
+                    decorated.GetWeather(city);
+                    decoratorSucceeded = true;
+                }
+                catch
+                {
+                    decoratorSucceeded = false;
+                }
+            }
+
+            Console.WriteLine("Not decorated provider failed.");
+            Console.WriteLine("Decorated provider succeeded!");
+
+            while (!baseProviderFailed & decoratorSucceeded)
+            {
+                try
+                {
+                    provider.GetWeather(city);
+                    baseProviderFailed = false;
+                }
+                catch
+                {
+                    baseProviderFailed = true;
+                }
+
+                try
+                {
+                    decorated.GetWeather(city);
+                    decoratorSucceeded = true;
+                }
+                catch
+                {
+                    decoratorSucceeded = false;
+                }
+            }
+
+            Console.WriteLine("\nNot decorated provider failed.");
+            Console.WriteLine("Decorated provider failed too!");
+
+            Console.ReadLine();
         }
     }
 }
